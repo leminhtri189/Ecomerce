12886c9fc3014a2fe0d399f3b51871522f54577d
Lê Minh Trí <trilm189@gmail.com>
2024-09-23 01:11:12 +0700

    Add project files.

---
+++ Ecommerce/ViewComponents/MenuLoaiComponent.cs
@@ -0,0 +1,23 @@
+﻿using Ecommerce.Data;
+using Ecommerce.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+[ViewComponent(Name = "MenuLoai")]
+public class MenuLoaiComponent : ViewComponent
+{
+    private readonly Hshop2023Context db;
+
+    public MenuLoaiComponent(Hshop2023Context context) => db = context;
+
+    public IViewComponentResult Invoke()
+    {
+        var data = db.Loais.Select(lo => new MenuLoaiVM
+        {
+            Maloai = lo.MaLoai,
+            Tenloai = lo.TenLoai,
+            SoLuong = lo.HangHoas.Count
+        }).OrderBy(p => p.Tenloai);
+        return View(data);
+    }
+
+}
+
---
+++ Ecommerce/ViewModels/MenuLoaiVM.cs
@@ -0,0 +1,9 @@
+﻿namespace Ecommerce.ViewModels
+{
+    public class MenuLoaiVM
+    {
+        public int Maloai{ get; set; }
+        public string Tenloai{ get; set; }
+        public  int SoLuong { get; set; }
+    }
+}
